97776acce8a858c48912a94fed3f76cb26a0d484
Joe <joegezo@gmail.com>
2023-04-11 20:36:37 -0400

    diagnosing github issue

--- tests/test_electricmayhem.py
+++ tests/test_electricmayhem.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 """Tests for `electricmayhem` package."""
 
--- tests/test_perlin.py
+++ tests/test_perlin.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np 
 import torch
 
 from electricmayhem._perlin import (perlin, 
