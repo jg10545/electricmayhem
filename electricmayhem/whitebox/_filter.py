eb633b427b3ad6806a4d2b81fd12d8f943291755
Joe <joegezo@gmail.com>
2024-08-18 19:40:25 -0400

    multi patch option for highpass filter

--- electricmayhem/whitebox/_filter.py
+++ electricmayhem/whitebox/_filter.py
@@ -17,16 +17,19 @@ class HighPassFilter(PipelineBase):
     """
     name = "HighPassFilter"
     
-    def __init__(self, limit=None, limit_x=None, limit_y=None):
+    def __init__(self, limit=None, limit_x=None, limit_y=None, keys=None):
         """
         :limit: int; frequency limit in pixels
         :limit_x: int; alternate to limit- specify frequency cutoff in x-axis
         :limit_y: int; alternate to limit- specify frequency cutoff in y axis
+        :keys: if using multiple patches and only applying the filter to some, pass a list of 
+            strings here to specify which ones
         """
         super().__init__()
         if limit is not None:
             limit_x = limit
             limit_y = limit
+        self.keys = keys
             
         assert (limit_x is not None)&(limit_y is not None), "need to specify frequency cutoff"
         
@@ -34,11 +37,17 @@ class HighPassFilter(PipelineBase):
             "limit_x":limit_x,
             "limit_y":limit_y
         }
+        if keys is not None:
+            self.params["keys"] = keys
         
     def forward(self, x, control=False, evaluate=False, params={}, **kwargs):
         """
         Run image through highpass filter; for evaluation steps do nothing
         """
+        # multi-patch case
+        if isinstance(x, dict):
+            return self._apply_forward_to_dict(x, control=control, evaluate=evaluate, 
+                                               params=params, **kwargs)
         if evaluate:
             return x, kwargs
         else:
