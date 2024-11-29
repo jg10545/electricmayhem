ff05257efbda3c665d10f498c6b0d3da4fab2f89
Joe <joegezo@gmail.com>
2024-11-29 09:46:07 -0500

    took ONNX stuff back out. absolute dumpster fire

--- electricmayhem/whitebox/__init__.py
+++ electricmayhem/whitebox/__init__.py
@@ -3,7 +3,7 @@ from ._implant import ScaleToBoxRectanglePatchImplanter
 from ._pipeline import Pipeline, ModelWrapper
 from ._aug import KorniaAugmentationPipeline
 from ._create import PatchResizer, PatchStacker, PatchSaver, PatchTiler, PatchScroller
-from ._yolo import YOLOWrapper, load_yolo_from_onnx
+from ._yolo import YOLOWrapper
 from ._proofer import SoftProofer
 from ._warp_implant import WarpPatchImplanter
 from ._filter import HighPassFilter
